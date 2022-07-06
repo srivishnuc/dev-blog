b2ebb8cf57a5c38452c3ef77a0eebfc73a20377f
srivishnuc <srivishnu995@gmail.com>
2022-07-06 22:11:33 +0530

    Side tab added in content container

--- src/Container/Content.js
+++ src/Container/Content.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-
+import SideTab from './SideTab'
 
 
 function Content() {
     return (
         <div className='grid grid-cols-4 h-auto'>
+            <SideTab />
             <Outlet />
-        </div>
+        </div >
     )
 }
 
