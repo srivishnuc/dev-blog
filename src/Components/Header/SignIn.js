0d66a5ce568156c96480688995a120eef4a3f99b
srivishnuc <srivishnu995@gmail.com>
2022-07-11 22:23:54 +0530

    sign in onchange event value mapped with state

--- src/Components/Header/SignIn.js
+++ src/Components/Header/SignIn.js
@@ -1,6 +1,10 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 function SignIn() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    console.log({ username, password })
+
     return (
         <div className="col-start-10 col-span-6 max-w-xs m-10 p-4">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -8,13 +12,13 @@ function SignIn() {
                     <label className="block text-primary text-sm font-bold mb-2" htmlFor="username">
                         Username
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={e => setUsername(e.target.value)} id="username" type="text" placeholder="Username" value={username} />
                 </div>
                 <div className="mb-6">
                     <label className="block text-primary text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" onChange={e => setPassword(e.target.value)} id="password" type="password" placeholder="******************" value={password} />
                     <p className="text-error text-xs italic">Please enter the password.</p>
                 </div>
                 <div className="flex items-center justify-between">
