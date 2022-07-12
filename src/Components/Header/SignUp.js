370aaa2f08b0c425d02cbae9079cab4cedba17d4
srivishnuc <srivishnu995@gmail.com>
2022-07-12 22:59:48 +0530

    signup onchange event value mapped with state

--- src/Components/Header/SignUp.js
+++ src/Components/Header/SignUp.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 function SignUp() {
     let navigate = useNavigate()
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [cpassword, setCpassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [phone, setPhone] = useState("")
 
+    console.log({ username, password, cpassword, email, phone })
     const handleClick = () => {
         navigate("/blog", { replace: true });
     }
@@ -16,33 +22,33 @@ function SignUp() {
                     <label className="block text-primary text-sm font-bold mb-2" htmlFor="username">
                         Username
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} value={username} />
                 </div>
                 <div className="mb-6">
                     <label className="block text-primary text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" onChange={e => setPassword(e.target.value)} value={password} />
                     <p className="text-error text-xs italic">Please enter the password.</p>
                 </div>
                 <div className="mb-6">
                     <label className="block text-primary text-sm font-bold mb-2" htmlFor="confirmpassword">
                         Confirm Password
                     </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirmpassword" type="password" placeholder="******************" />
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirmpassword" type="password" placeholder="******************" onChange={e => setCpassword(e.target.value)} value={cpassword} />
                     <p className="text-error text-xs italic">Please re-enter the password.</p>
                 </div>
                 <div className="mb-6">
                     <label className="block text-primary text-sm font-bold mb-2" htmlFor="email">
                         Email
                     </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="xyz@test.com" />
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="xyz@test.com" onChange={e => setEmail(e.target.value)} value={email} />
                 </div>
                 <div className="mb-6">
                     <label className="block text-primary text-sm font-bold mb-2" htmlFor="phone">
                         Phone
                     </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="number" placeholder="9876543210" />
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="number" placeholder="9876543210" onChange={e => setPhone(e.target.value)} value={phone} />
 
                 </div>
                 <div className="flex items-center justify-end">
